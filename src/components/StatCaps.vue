a0c8dd73982d593f9cdf5c6823f7a0e7edb14ea2
Accomp <exaltedsolutions@gmail.com>
2017-12-30 00:03:55 -0500

    Initial commit

---
+++ src/components/StatCaps.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-data-table
+    v-bind:headers="headers"
+    :items="stats"
+    hide-actions
+    class="elevation-1"
+  >
+    <template slot="items" slot-scope="props">
+      <td>{{ props.item.name }}</td>
+      <td class="text-xs-right">{{ props.item.t1 }}</td>
+      <td class="text-xs-right">{{ props.item.t2 }}</td>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  name: 'StatCaps',
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Stats',
+          align: 'left',
+          sortable: false,
+          value: 'name'
+        },
+        {
+          text: 'T1',
+          sortable: false,
+          value: 't1'
+        },
+        {
+          text: 'T2',
+          sortable: false,
+          value: 't2'
+        }
+      ],
+      stats: [
+        {
+          value: false,
+          name: 'Armour',
+          t1: 12000,
+          t2: 17000
+        },
+        {
+          value: false,
+          name: 'Mitigation',
+          t1: 550,
+          t2: 640
+        },
+        {
+          value: false,
+          name: 'Parry',
+          t1: 1100,
+          t2: 1499
+        },
+      ]
+    };
+  },
+};
+</script>
